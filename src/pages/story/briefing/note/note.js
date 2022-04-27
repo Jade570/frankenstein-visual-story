a051af1097a2a9aed0eb67e66a56a1b0114cf211
jade570 <chaelyoung@naver.com>
2022-04-27 22:02:58 +0900

    add image in BriefNote

--- src/pages/story/briefing/note/note.js
+++ src/pages/story/briefing/note/note.js
@@ -1,21 +1,24 @@
 import { React, useState } from "react";
 
 const BriefNote = (props) => {
-  const cctvThumbnail = props.json.cctv.map((x) => {
-    return (
-      <div>
-        <div>촬영 장소: {x.place}</div>
-        {<img src={x.thumbnail}></img>}
-      </div>
-    );
-  });
-  console.log(cctvThumbnail);
   if (props.showBriefNote) {
     return (
       <div className="briefNote">
         <div>장소: {props.json.explanation.place}</div>
         <div>종류: {props.json.explanation.case}</div>
-        {cctvThumbnail.map((x) => x)}
+        <div>
+          {props.json.cctv.map((item, idx) => {
+            const imageURI = require(`${item.thumbnail}`);
+            const name = item.place;
+            return (
+              <div style={{ textAlign: "center" }}>
+                <div>{name}</div>
+                <img src={imageURI} width={200} />
+                <br />
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   } else {
