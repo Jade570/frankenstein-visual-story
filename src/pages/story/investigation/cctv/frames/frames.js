2118b1958ca4999d7350ed7419ffc004e42a731c
jade570 <chaelyoung@naver.com>
2022-05-02 21:19:16 +0900

    Update story

--- src/pages/story/investigation/cctv/frames/frames.js
+++ src/pages/story/investigation/cctv/frames/frames.js
@@ -2,11 +2,15 @@ import { React } from "react";
 import "./frames.css";
 
 const Frames = (props) => {
-  console.log(props.data.objects[props.frame]);
+  const object = props.object.filter((c, index) => {
+    return props.object.indexOf(c) === index;
+  });
+  object.sort();
+  console.log(object);
   return (
     <div className="container">
       <img className="background" src={props.background}></img>
-      <img className="objects" src={props.object[props.frame]}></img>
+      <img className="objects" src={object[props.frame]}></img>
     </div>
   );
 };
