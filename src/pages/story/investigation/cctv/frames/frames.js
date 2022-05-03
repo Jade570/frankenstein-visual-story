a9ba779c80805aabfe5c33ceaf5f440340088877
jade570 <chaelyoung@naver.com>
2022-05-03 22:57:53 +0900

    Update frame

--- src/pages/story/investigation/cctv/frames/frames.js
+++ src/pages/story/investigation/cctv/frames/frames.js
@@ -25,29 +25,31 @@ const Frames = (props) => {
   };
 
   return (
-    <div className="container">
-      <img className="background" src={props.background}></img>
-      <img
-        className="objects"
-        src={object[props.frame]}
-        onClick={closeAll}
-      ></img>
-      <div
-        className="suggestion1"
-        onClick={handleSuggestion1}
-        style={{
-          gridColumn: props.grid1[props.frame].column,
-          gridRow: props.grid1[props.frame].row,
-        }}
-      ></div>
-      <div
-        className="suggestion2"
-        onClick={handleSuggestion2}
-        style={{
-          gridColumn: props.grid2[props.frame].column,
-          gridRow: props.grid2[props.frame].row,
-        }}
-      ></div>
+    <div>
+      <div className="container">
+        <img className="background" src={props.background}></img>
+        <img
+          className="objects"
+          src={object[props.frame]}
+          onClick={closeAll}
+        ></img>
+        <div
+          className="suggestion1"
+          onClick={handleSuggestion1}
+          style={{
+            gridColumn: props.grid1[props.frame].column,
+            gridRow: props.grid1[props.frame].row,
+          }}
+        ></div>
+        <div
+          className="suggestion2"
+          onClick={handleSuggestion2}
+          style={{
+            gridColumn: props.grid2[props.frame].column,
+            gridRow: props.grid2[props.frame].row,
+          }}
+        ></div>
+      </div>
       <MyModal
         isOpen={sugg1}
         suggestion={props.suggestion1}
