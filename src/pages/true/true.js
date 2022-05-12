c9fdad2124cbc6846ebaccb5383d8b54c98dfcaa
jade570 <chaelyoung@naver.com>
2022-05-13 04:13:42 +0900

    Update true ending style

--- src/pages/true/true.js
+++ src/pages/true/true.js
@@ -2,13 +2,28 @@ import React from "react";
 
 const True = (props) => {
   return (
-    <div className="True">
-      <video controls width="1080">
-        <source
-          src="https://firebasestorage.googleapis.com/v0/b/frankenstein-visual-story.appspot.com/o/Ending%20Video%20-%202nd%20draft.mp4?alt=media&token=f7f49c8c-92e9-4d6c-8795-57f0ffee74d8"
-          type="video/mp4"
-        ></source>
-      </video>
+    <div
+      className="True"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+      }}
+    >
+      <div>
+        {" "}
+        <video
+          style={{
+            width: "100%",
+            height: "auto",
+          }}
+        >
+          <source
+            src="https://firebasestorage.googleapis.com/v0/b/frankenstein-visual-story.appspot.com/o/Ending%20Video%20-%202nd%20draft.mp4?alt=media&token=f7f49c8c-92e9-4d6c-8795-57f0ffee74d8"
+            type="video/mp4"
+          ></source>
+        </video>
+      </div>
     </div>
   );
 };
